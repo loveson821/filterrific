432a34a626f5a94bbb6ff8888fa28805422f5347
loveson821 <loveson821@gmail.com>
2021-11-11 16:18:24 +0800

    comment out version check

--- lib/filterrific.rb
+++ lib/filterrific.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-if ![5,6].include?(Rails::VERSION::MAJOR)
-  raise "\n\nThis version of Filterrific only works with Rails 5 and 6.\nPlease see the Filterrific README for the correct version of Filterrific to use with your version of Rails!\n\n"
-end
+# if ![5,6].include?(Rails::VERSION::MAJOR)
+#   raise "\n\nThis version of Filterrific only works with Rails 5 and 6.\nPlease see the Filterrific README for the correct version of Filterrific to use with your version of Rails!\n\n"
+# end
 
 require 'filterrific/version'
 require 'filterrific/engine'
